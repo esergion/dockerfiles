9520f36b91142a5d1bd5864ffda7c8a890ac87a0
Sergey Pyankov <esergion@gmail.com>
2015-09-16 12:03:21 +0500

    Update start_pgsql.sh

--- dokku-alt-postgresql/start_pgsql.sh
+++ dokku-alt-postgresql/start_pgsql.sh
@@ -9,7 +9,7 @@ fi
 if [[ ! -f /opt/postgresql/initialized ]]; then
     mkdir -p /opt/postgresql
     pg_dropcluster --stop 9.1 main
-    pg_createcluster --locale ru_RU.UTF-8 9.1. main
+    pg_createcluster --locale ru_RU.UTF-8 9.1 main
     cp -a /var/lib/postgresql/* /opt/postgresql/
     chown -R postgres:postgres /opt/postgresql
     DB_PASSWORD="$(cat "/opt/postgresql_password")"
